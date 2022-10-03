38f97cd8357f4911b1855761f1f53415d16abedf
tikkanipraveen <tikkani.praveen@gmail.com>
2022-10-03 16:32:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfas´t Menu')
+streamlit.text('Omega3 & Blueberry Oatmwal')
+streamlit.text('Kale Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
