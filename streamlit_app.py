5c4c4f02b19d7992971f7509c4b149914baba612
tikkanipraveen <tikkani.praveen@gmail.com>
2022-10-03 16:36:26 +0200

    Added emojis and breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry Oatmwal')
-streamlit.text('Kale Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega3 & Blueberry Oatmwal')
+streamlit.text('🥗 Kale Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
