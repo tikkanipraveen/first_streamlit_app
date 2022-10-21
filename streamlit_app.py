1f945b5f2c0c8bbebedaa097cc2d8a2eb1c19449
tikkanipraveen <tikkani.praveen@gmail.com>
2022-10-21 16:48:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,17 +48,20 @@ streamlit.write('The user entered ', fruit_choice)
 # display in the screen as a table
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
 
-#importing snowflake connector id
-#import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#snowflake-related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur
+   my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+   return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_rows = get_fruit_load_list()
+     streamlit.dataframe(my_data_rows)
+streamlit.stop()
 
 #new section to display fruityvice api response
 add_my_fruit = streamlit.text_input('What fruit you would like to add?','jackfruit')
